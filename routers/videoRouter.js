91164801c41d44ce7695c42fdef28dc3e3923e7c
front3ndninja <niloy98.it@gmail.com>
2021-03-25 21:47:21 +0600

    added video router

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,25 @@
 const express = require("express");
-
+const routes = require("../routers");
 const videoRouter = express.Router();
+
+videoRouter.get(routes.videos, (req, res) => {
+  res.send("videos");
+});
+
+videoRouter.get(routes.upload, (req, res) => {
+  res.send("upload");
+});
+
+videoRouter.get(routes.videoDetail, (req, res) => {
+  res.send("video details");
+});
+
+videoRouter.get(routes.editVideo, (req, res) => {
+  res.send("edit video");
+});
+
+videoRouter.get(routes.deleteVideo, (req, res) => {
+  res.send("delete video");
+});
 
 module.exports = videoRouter;
