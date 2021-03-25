40b8d86b075effac987bea85266dea032ffe17d1
front3ndninja <niloy98.it@gmail.com>
2021-03-25 21:49:12 +0600

    added global router

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,5 +1,25 @@
 const express = require("express");
-
+const routes = require("../routers");
 const globalRouter = express.Router();
+
+globalRouter.get(routes.home, (req, res) => {
+  res.send("hello from global home");
+});
+
+globalRouter.get(routes.join, (req, res) => {
+  res.send("hello from global join");
+});
+
+globalRouter.get(routes.login, (req, res) => {
+  res.send("hello from global login");
+});
+
+globalRouter.get(routes.logout, (req, res) => {
+  res.send("hello from global logout");
+});
+
+globalRouter.get(routes.search, (req, res) => {
+  res.send("hello from global search");
+});
 
 module.exports = globalRouter;
