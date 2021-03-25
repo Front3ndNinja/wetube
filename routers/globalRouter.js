bccc327d0b090819901227b9675f415d156d69ba
front3ndninja <niloy98.it@gmail.com>
2021-03-25 22:53:03 +0600

    adapted to controller

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,25 +1,17 @@
 const express = require("express");
+const { join, login, logout } = require("../controllers/userController");
+const { home, search } = require("../controllers/videoController");
 const routes = require("../routers");
 const globalRouter = express.Router();
 
-globalRouter.get(routes.home, (req, res) => {
-  res.send("hello from global home");
-});
+globalRouter.get(routes.home, home);
 
-globalRouter.get(routes.join, (req, res) => {
-  res.send("hello from global join");
-});
+globalRouter.get(routes.join, join);
 
-globalRouter.get(routes.login, (req, res) => {
-  res.send("hello from global login");
-});
+globalRouter.get(routes.login, login);
 
-globalRouter.get(routes.logout, (req, res) => {
-  res.send("hello from global logout");
-});
+globalRouter.get(routes.logout, logout);
 
-globalRouter.get(routes.search, (req, res) => {
-  res.send("hello from global search");
-});
+globalRouter.get(routes.search, search);
 
 module.exports = globalRouter;
