07af218105a18e566eede1ea93917ba2ae6e6685
front3ndninja <niloy98.it@gmail.com>
2021-03-25 22:51:38 +0600

    updated user controller

---
+++ controllers/userController.js
@@ -0,0 +1,27 @@
+export const join = (req, res) => {
+  res.send("join");
+};
+
+export const login = (req, res) => {
+  res.send("login");
+};
+
+export const logout = (req, res) => {
+  res.send("logout");
+};
+
+export const user = (req, res) => {
+  res.send("user");
+};
+
+export const userDetail = (req, res) => {
+  res.send("user Detail 1");
+};
+
+export const editProfile = (req, res) => {
+  res.send("editProfile");
+};
+
+export const changePassword = (req, res) => {
+  res.send("changePassword");
+};
