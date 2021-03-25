d959b7c7379beb11ad2583aef33cdfa678d1d203
front3ndninja <niloy98.it@gmail.com>
2021-03-25 21:59:02 +0600

    added user routers

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const routes = require("../routers");
 
 const userRouter = express.Router();
 
+userRouter.get(routes.users, (req, res) => {
+  res.send("users");
+});
+userRouter.get(routes.userDetail, (req, res) => {
+  res.send("userDetail");
+});
+userRouter.get(routes.editProfile, (req, res) => {
+  res.send("editProfile");
+});
+userRouter.get(routes.changePassword, (req, res) => {
+  res.send("changePassword");
+});
 module.exports = userRouter;
