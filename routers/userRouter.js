785801a87a3e32f3f876b4cb7c9286ca2120b09e
front3ndninja <niloy98.it@gmail.com>
2021-03-27 02:50:05 +0600

    added post request

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -10,8 +10,9 @@ const routes = require("../routers");
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, user);
-userRouter.get(routes.userDetail, userDetail);
+// userRouter.get(routes.users, user);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail(), userDetail);
+
 module.exports = userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,24 +1,21 @@
 const express = require("express");
 const {
-  videos,
-  upload,
   videoDetail,
   deleteVideo,
   editVideo,
+  getUpload,
+  postUpload,
 } = require("../controllers/videoController");
 
 const routes = require("../routers");
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
 
-videoRouter.get(routes.upload, upload);
-
-videoRouter.get(routes.videoDetail, videoDetail);
-
-videoRouter.get(routes.editVideo, editVideo);
-
+videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.editVideo, editVideo);
 
 module.exports = videoRouter;
