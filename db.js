9be617235f35187e651f160c68a416d37f21e971
front3ndninja <niloy98.it@gmail.com>
2021-03-27 02:29:16 +0600

    added dummy data

---
+++ db.js
@@ -0,0 +1,43 @@
+const videos = [
+  {
+    id: 343434,
+    title: "test videos 1",
+    description: "test video number 1",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 2342342,
+      name: "Test User",
+      email: "test@gmail.com",
+    },
+  },
+  {
+    id: 343434,
+    title: "test videos 2",
+    description: "test video number 1",
+    views: 27,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 2342354,
+      name: "Test User",
+      email: "test@gmail.com",
+    },
+  },
+  {
+    id: 34343664,
+    title: "test videos 3",
+    description: "test video number 1",
+    views: 89,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 2342342,
+      name: "Test User",
+      email: "test@gmail.com",
+    },
+  },
+];
+
+module.exports = videos;
