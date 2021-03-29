53a093e0964cbde0d36abcd878368c1f698ec997
front3ndninja <niloy98.it@gmail.com>
2021-03-29 17:29:26 +0600

    change to mongo

--- init.js
+++ init.js
@@ -1,6 +1,8 @@
+import "./db";
 import app from "./app";
-
-const PORT = 4000;
+import "./models/Video";
+import "./models/Comment";
+const PORT = process.env.PORT || 4000;
 
 function handleListening() {
   console.log(`listening on : ${PORT}`);
